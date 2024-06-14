4c4f53f482d87a308cc4ae5b71966a78ccfb3073
Test 2 <Test2@agpl.net>
2024-06-14 11:26:03 +0530

    navbar

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Zitow</title>
+    <link rel="stylesheet" href="{{ asset('user/assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('user/assets/css/Navbar-Right-Links-icons.css') }}">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md bg-primary py-3">
+        <div class="container-fluid"><a class="navbar-brand d-flex align-items-center"
+                href="#"><span>Zitow</span></a><button data-bs-toggle="collapse" class="navbar-toggler"
+                data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span
+                    class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="#">About Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Contact Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Pricing</a></li>
+                </ul><button class="btn btn-danger" type="button">Sign Up</button>
+            </div>
+        </div>
+    </nav>
+    <script src="{{ asset('user/assets/bootstrap/js/bootstrap.min.js') }}"></script>
+</body>
+
+</html>
